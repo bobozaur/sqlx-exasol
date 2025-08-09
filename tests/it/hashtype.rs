68a147acf5579de6141383854171f6f3793d523a
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2025-08-10 01:22:19 +0300

    fix hashtype tests

--- tests/it/hashtype.rs
+++ tests/it/hashtype.rs
@@ -2,6 +2,6 @@ use sqlx_exasol::types::HashType;
 
 use crate::{test_type_array, test_type_valid};
 
-test_type_valid!(hashtype<HashType>::"HASHTYPE (15 BYTE)"::("'550e8400e29b11d4a7164466554400'" => HashType(String::from("550e8400e29b11d4a716446655440000"))));
+test_type_valid!(hashtype<HashType>::"HASHTYPE (15 BYTE)"::("'550e8400e29b11d4a7164466554400'" => HashType(String::from("550e8400e29b11d4a7164466554400"))));
 
 test_type_array!(hashtype_array<HashType>::"HASHTYPE (15 BYTE)"::(vec![HashType(String::from("550e8400e29b11d4a7164466554400")), HashType(String::from("550e8400e29b11d4a7164466554400"))]));
